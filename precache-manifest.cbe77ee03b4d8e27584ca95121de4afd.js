c72369c92aec23f20d8d9557340996f0a4c72cdc
Nikita Zhelonkin <niks.mbox@gmail.com>
2020-01-17 18:43:45 +0300

    Updates

---
+++ precache-manifest.cbe77ee03b4d8e27584ca95121de4afd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f889ae10d3fd58205a705dcce1b78b9c",
+    "url": "/BonusCards/index.html"
+  },
+  {
+    "revision": "7180b1d081e00ed659fc",
+    "url": "/BonusCards/static/css/2.0a8dee31.chunk.css"
+  },
+  {
+    "revision": "d7a647f357cee99a27c1",
+    "url": "/BonusCards/static/css/main.fb44e015.chunk.css"
+  },
+  {
+    "revision": "7180b1d081e00ed659fc",
+    "url": "/BonusCards/static/js/2.966d5f03.chunk.js"
+  },
+  {
+    "revision": "d7a647f357cee99a27c1",
+    "url": "/BonusCards/static/js/main.6a58edbf.chunk.js"
+  },
+  {
+    "revision": "ed2247b58b16b66150bc",
+    "url": "/BonusCards/static/js/runtime-main.915cdbb2.js"
+  },
+  {
+    "revision": "6d6249dddbf5f624fbe1a9b6861b1270",
+    "url": "/BonusCards/static/media/barcode.6d6249dd.svg"
+  },
+  {
+    "revision": "f8d6a335a4fd2ffab4e58478d71959e3",
+    "url": "/BonusCards/static/media/barcode_how.f8d6a335.png"
+  }
+]);

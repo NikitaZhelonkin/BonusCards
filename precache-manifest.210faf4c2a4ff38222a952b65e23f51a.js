3ce6551efb229dc8d7153931c1dad1d11a4d739f
Nikita Zhelonkin <niks.mbox@gmail.com>
2020-01-30 12:41:34 +0300

    Updates

--- precache-manifest.210faf4c2a4ff38222a952b65e23f51a.js
+++ precache-manifest.210faf4c2a4ff38222a952b65e23f51a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbdd4e54e5e82e354ea98723c1ef126d",
+    "revision": "9b425cf08bff14cbb63d1fac850405bd",
     "url": "/BonusCards/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BonusCards/static/css/2.0a8dee31.chunk.css"
   },
   {
-    "revision": "088372df4196d40949f0",
+    "revision": "fd67faef7b322552bdef",
     "url": "/BonusCards/static/css/main.fb44e015.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/BonusCards/static/js/2.97b6e8cb.chunk.js"
   },
   {
-    "revision": "088372df4196d40949f0",
-    "url": "/BonusCards/static/js/main.a9108bab.chunk.js"
+    "revision": "fd67faef7b322552bdef",
+    "url": "/BonusCards/static/js/main.e3db375d.chunk.js"
   },
   {
     "revision": "ed2247b58b16b66150bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BonusCards/precache-manifest.a4822d3dd93084b221c215d8707fae40.js"
+  "/BonusCards/precache-manifest.210faf4c2a4ff38222a952b65e23f51a.js"
 );
 
 self.addEventListener('message', (event) => {
